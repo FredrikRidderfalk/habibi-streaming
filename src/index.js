486de771906f26b059c28ba69514a7d2a4571ecd
Fredrik Ridderfalk <ridderfalk@gmail.com>
2021-11-26 12:41:36 +0100

    added footer container

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { GlobalStyles } from "./global-styles";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <>
+    <GlobalStyles />
+    <App />
+  </>,
+  document.getElementById("root")
+);
 
 reportWebVitals();
