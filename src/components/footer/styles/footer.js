815522eca05fa9f39269e21645ea65c609acf587
Fredrik Ridderfalk <ridderfalk@gmail.com>
2021-11-26 12:58:54 +0100

    wrote out footer columns

--- src/components/footer/styles/footer.js
+++ src/components/footer/styles/footer.js
@@ -4,22 +4,14 @@ export const Container = styled.div`
   display: flex;
 `;
 
-export const Row = styled.div`
-  display: flex;
-`;
+export const Row = styled.div``;
 
-export const Column = styled.div`
-  display: flex;
-`;
+export const Column = styled.div``;
 
-export const Link = styled.div`
-  display: flex;
-`;
+export const Link = styled.a``;
 
-export const Title = styled.div`
-  display: flex;
-`;
+export const Title = styled.p``;
 
-export const Break = styled.div`
-  display: flex;
-`;
+export const Text = styled.p``;
+
+export const Break = styled.div``;
