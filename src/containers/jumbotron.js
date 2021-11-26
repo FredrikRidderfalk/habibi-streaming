495a66bdfd6a1f0226e3cb49a56d9caa55e13c7e
Fredrik Ridderfalk <ridderfalk@gmail.com>
2021-11-26 09:01:46 +0100

    moved jumbotron title and subtitle

---
+++ src/containers/jumbotron.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Jumbotron from "../components/jumbotron";
+
+export function JumbotronContainer() {
+  return (
+    <Jumbotron.Container>
+      <Jumbotron.Title>Hello habiibiiiii</Jumbotron.Title>
+      <Jumbotron.SubTitle>
+        Hello habiibiiiii in the subtitle!!
+      </Jumbotron.SubTitle>
+    </Jumbotron.Container>
+  );
+}
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,6 @@
 import React from "react";
-import Jumbotron from "../components/jumbotron";
+import { JumbotronContainer } from "../containers/jumbotron";
 
 export default function Home() {
-  return (
-    <Jumbotron.Container>
-      <Jumbotron.Title>Hello habiibiiiii</Jumbotron.Title>
-      <Jumbotron.SubTitle>
-        Hello habiibiiiii in the subtitle!!
-      </Jumbotron.SubTitle>
-    </Jumbotron.Container>
-  );
+  return <JumbotronContainer />;
 }
