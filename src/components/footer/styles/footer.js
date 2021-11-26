7a42773211b020d6a4b78a75b8ed684e6d0e8386
Fredrik Ridderfalk <ridderfalk@gmail.com>
2021-11-26 12:49:18 +0100

    added components to footer

--- src/components/footer/styles/footer.js
+++ src/components/footer/styles/footer.js
@@ -3,3 +3,23 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
 `;
+
+export const Row = styled.div`
+  display: flex;
+`;
+
+export const Column = styled.div`
+  display: flex;
+`;
+
+export const Link = styled.div`
+  display: flex;
+`;
+
+export const Title = styled.div`
+  display: flex;
+`;
+
+export const Break = styled.div`
+  display: flex;
+`;
