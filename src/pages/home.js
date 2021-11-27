91404b8a01c87444ef82cee888d74fa2f6433b71
Fredrik Ridderfalk <ridderfalk@gmail.com>
2021-11-27 12:39:18 +0100

    added feature component

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Feature, OptForm } from "../components";
 import { FaqsContainer } from "../containers/faqs";
 import { JumbotronContainer } from "../containers/jumbotron";
 import { FooterContainer } from "../containers/footer";
@@ -8,7 +9,23 @@ export default function Home() {
   return (
     <>
       <HeaderContainer>
-        <p>Habibiiiiijiiiui</p>
+        <OptForm>
+          <Feature>
+            <Feature.Title>
+              Unlimited films, TV programmes and more.
+            </Feature.Title>
+            <Feature.SubTitle>
+              Watch anywhere. Cancel at any time.
+            </Feature.SubTitle>
+            <OptForm.Input placeholder="Email Address" />
+            <OptForm.Button>Try it now</OptForm.Button>
+            <OptForm.Break />
+            <OptForm.Text>
+              Ready to watch? Enter your email to create or restart your
+              membership.
+            </OptForm.Text>
+          </Feature>
+        </OptForm>
       </HeaderContainer>
 
       <JumbotronContainer />
