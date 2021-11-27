db350ca41aef3e9c70df4100282ce43c9e8d090f
Fredrik Ridderfalk <ridderfalk@gmail.com>
2021-11-27 17:46:41 +0100

    commit before initializing firebase store

--- src/index.js
+++ src/index.js
@@ -3,11 +3,26 @@ import ReactDOM from "react-dom";
 import { GlobalStyles } from "./global-styles";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { FirebaseContext } from "./context/firebase";
+// import { initializeApp } from "firebase/app";
+
+const config = {
+  apiKey: "AIzaSyCK8KhTKjSP-M6Fg2YMg3Hqm_EayNb2_tE",
+  authDomain: "habibi-streaming.firebaseapp.com",
+  projectId: "habibi-streaming",
+  storageBucket: "habibi-streaming.appspot.com",
+  messagingSenderId: "212948137795",
+  appId: "1:212948137795:web:0d04a0f44b3368fd9d61f6",
+};
+
+const firebase = window.firebase.initializeApp(config);
 
 ReactDOM.render(
   <>
-    <GlobalStyles />
-    <App />
+    <FirebaseContext.Provider value={{ firebase: window.firebase }}>
+      <GlobalStyles />
+      <App />
+    </FirebaseContext.Provider>
   </>,
   document.getElementById("root")
 );
