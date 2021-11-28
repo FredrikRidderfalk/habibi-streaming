ffff02701057e02903a0eb4567e7ea4e2c4604f2
Fredrik Ridderfalk <ridderfalk@gmail.com>
2021-11-28 20:12:24 +0100

    added feature component to browse page

--- src/containers/header.js
+++ src/containers/header.js
@@ -9,7 +9,7 @@ export function HeaderContainer({ children }) {
         <Header.Logo
           to={ROUTES.HOME}
           src="/images/misc/logo.svg"
-          alt="Netflix"
+          alt="Habibi"
         />
         <Header.ButtonLink to={ROUTES.SIGN_IN}>Sign In</Header.ButtonLink>
       </Header.Frame>
