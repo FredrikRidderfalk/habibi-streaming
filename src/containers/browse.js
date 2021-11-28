be331747ad27e2a2dc93c0af223f78aa548476d8
Fredrik Ridderfalk <ridderfalk@gmail.com>
2021-11-28 14:36:15 +0100

    added browser page and container

---
+++ src/containers/browse.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Header } from "../components";
+import * as ROUTES from "../constants/routes";
+import { FirebaseContext } from "../context/firebase";
+import { SelectProfileContainer } from "./profiles";
+
+export function BrowseContainer() {
+  return <p>Browse Container</p>;
+}
