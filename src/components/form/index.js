5cb6c526077945bbde66300989889e96883ee425
Fredrik Ridderfalk <ridderfalk@gmail.com>
2021-11-28 11:12:47 +0100

    added subparts of form

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -4,3 +4,23 @@ import { Container, Base, Title, Error, Input, Submit } from "./styles/form";
 export default function Form({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
 }
+
+Form.Base = function FormBase({ children, ...restProps }) {
+  return <Base {...restProps}>{children}</Base>;
+};
+
+Form.Title = function FormTitle({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>;
+};
+
+Form.Error = function FormError({ children, ...restProps }) {
+  return <Error {...restProps}>{children}</Error>;
+};
+
+Form.Input = function FormInput({ children, ...restProps }) {
+  return <Input {...restProps}>{children}</Input>;
+};
+
+Form.Submit = function FormSubmit({ children, ...restProps }) {
+  return <Submit {...restProps}>{children}</Submit>;
+};
